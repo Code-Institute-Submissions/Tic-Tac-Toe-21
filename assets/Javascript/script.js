298164839eca7274245cbe4e4d4d32c5b0323975
T-Kingston <thomasjkingston@icloud.com>
2022-05-28 18:11:11 +0000

    declared 'let' variables for the game board, which player is currently playing and set 'isGameActive' to true. Commented in a visual representation of the game board index and used that to set a constant variable for the games 'Winning Conditions'. Made each tile clickable using an event listener.

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -4,5 +4,14 @@ window.addEventListener('DOMContentLoaded', () => {
     const resetButton = document.querySelector('#reset-btn');
     const announcer = document.querySelector('.announcer');
 
+    let board = ['', '', '','', '', '','', '', ''];
+    let currentPlayer = 'X';
+    let isGameActive = true;
+
+    // declaring end game statments 
+    const PLAYERX_Won = 'PlayerX_Won'
+    const PLAYERO_Won = 'PlayerO_Won'
+    const TIE = 'It\'s a Tie'
+
     resetButton.addEventListener('click', resetBoard);
 });

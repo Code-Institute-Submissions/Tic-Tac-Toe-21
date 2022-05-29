e8ffd5b3d69526b4e821ad6b552e06f108ad90fa
T-Kingston <thomasjkingston@icloud.com>
2022-05-29 12:56:38 +0000

    added a switch case within an 'announce type' to announce which player has wonn, or if a tie. Also added a 'for' loop to check the winning conditions using ternary conditions and logical operators which keeps looping if conditions haven't been met, and returns when they have.

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,16 +1,21 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const tiles = Array.from(document.querySelectorAll('.tile'));
-    const playerDisplay = document.querySelector('.display-player');
-    const resetButton = document.querySelector('#reset-btn');
-    const announcer = document.querySelector('.announcer');
+    let tiles = Array.from(document.querySelectorAll('.tile'));
+    let resetButton = document.querySelector('#reset-btn');
+    let announcer = document.querySelector('.announcer');
 
     let board = ['', '', '','', '', '','', '', ''];
     let currentPlayer = 'X';
     let isGameActive = true;
 
-    // declaring end game statments 
-    const PLAYERX_Won = 'PlayerX_Won'
-    const PLAYERO_Won = 'PlayerO_Won'
+    tiles.forEach( (tile, index) => {
+        tile.addEventListener('click', () => userAction(tile, index));
+    });
+    resetButton.addEventListener('click', resetBoard);
+});
+   
+// declaring end game statments 
+    const PLAYERX_WON = 'PlayerX_Won'
+    const PLAYERO_WON = 'PlayerO_Won'
     const TIE = 'It\'s a Tie'
 
     /* 
@@ -20,7 +25,7 @@ window.addEventListener('DOMContentLoaded', () => {
         [6][7][8]
     */
 
-    const winningConditions = [
+    const WINNING_CONDITIONS = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8]
@@ -31,7 +36,49 @@ window.addEventListener('DOMContentLoaded', () => {
         [2, 4, 6]
     ];
 
-    const changePlayer = () => { 
+    // runs loop to see if game winning conditions have been met
+    function handleResultValidation() { 
+        let roundWon = false; 
+        for (let i=0; i <=7; i++) { 
+            const WIN_CONDITION = WinningConditions[i];
+            let a = board[WIN_CONDITION[0]];
+            let b = board[WIN_CONDITION[1]];
+            let c = board[WIN_CONDITION[2]];
+            if (a === '' || b === '' || c === '') { 
+                continue;
+            }
+            if (a === b && b === c) { 
+                roundWon = True;
+                break;
+            }
+        }
+    if (roundWon) { 
+        announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+        isGameActive = false;
+        return;
+    }
+    if (!board.includes(''));
+    announce(TIE);
+}
+
+    // announces which player has won
+    let announce = (type) => { 
+        switch(type) {
+            case PLAYERO_WON:
+                announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+                break;
+            case PLAYERX_WON:
+                announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+                break;
+            case TIE:
+                announcer.innerText = 'Tie';
+        }
+        announcer.classList.remove('hide');
+    };
+
+    // changes player based on which turn has just been taken
+    const CHANGE_PLAYER = () => { 
+        let playerDisplay = document.querySelector('.display-player');
         playerDisplay.classList.remove(`player${currentPlayer}`);
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
         playerDisplay.innerText = currentPlayer;
@@ -39,7 +86,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     
     // represents which user is currently playing. Checks whether game is active or not.
-    const userAction = (tile, index) => { 
+    const USER_ACTION = (tile, index) => { 
         if(isValidAction(tile) && isGameActive) { 
             tile.innerText = currentPlayer;
             tile.classList.add(`player${currentPlayer}`);
@@ -48,9 +95,3 @@ window.addEventListener('DOMContentLoaded', () => {
             changePlayer;
         }
     }
-
-    tiles.forEach( (tile, index) => {
-        tile.addEventListener('click', () => userAction(tile, index));
-    });
-    resetButton.addEventListener('click', resetBoard);
-});

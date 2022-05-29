22208204b7355a91993c702a6acd1c466652d992
T-Kingston <thomasjkingston@icloud.com>
2022-05-29 20:01:59 +0000

    remove DOMContentLoader as was creating scope blocking issues and was advised it was unnecessary. Repositioned event listeners for 'user action' and 'reset board' back to their original place at bottom of code. In HTML, moved the <script> tag back into the body section.

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,18 +1,12 @@
-window.addEventListener('DOMContentLoaded', () => {
     let tiles = Array.from(document.querySelectorAll('.tile'));
+    let playerDisplay = document.querySelector('.display-player');
     let resetButton = document.querySelector('#reset-btn');
     let announcer = document.querySelector('.announcer');
 
-    let board = ['', '', '','', '', '','', '', ''];
+    let board = ['', '', '', '', '', '', '', '', ''];
     let currentPlayer = 'X';
     let isGameActive = true;
 
-    tiles.forEach( (tile, index) => {
-        tile.addEventListener('click', () => userAction(tile, index));
-    });
-    resetButton.addEventListener('click', resetBoard);
-});
-   
 // declaring end game statments 
     const PLAYERX_WON = 'PlayerX_Won';
     const PLAYERO_WON = 'PlayerO_Won';
@@ -25,10 +19,10 @@ window.addEventListener('DOMContentLoaded', () => {
         [6][7][8]
     */
 
-    const WINNING_CONDITIONS = [
+    let winningConditions = [
         [0, 1, 2],
         [3, 4, 5],
-        [6, 7, 8]
+        [6, 7, 8],
         [0, 3, 6],
         [1, 4, 7],
         [2, 5, 8], 
@@ -39,31 +33,32 @@ window.addEventListener('DOMContentLoaded', () => {
     // runs loop to see if game winning conditions have been met
     function handleResultValidation() { 
         let roundWon = false; 
-        for (let i=0; i <=7; i++) { 
-            const WIN_CONDITION = WinningConditions[i];
-            let a = board[WIN_CONDITION[0]];
-            let b = board[WIN_CONDITION[1]];
-            let c = board[WIN_CONDITION[2]];
+        for (let i = 0; i <=7; i++) { 
+            let winCondition = winningConditions[i];
+            let a = board[winCondition[0]];
+            let b = board[winCondition[1]];
+            let c = board[winCondition[2]];
             if (a === '' || b === '' || c === '') { 
                 continue;
             }
             if (a === b && b === c) { 
-                roundWon = True;
+                roundWon = true;
                 break;
             }
         }
+    
     if (roundWon) { 
         announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
         isGameActive = false;
         return;
     }
     if (!board.includes(''));
-    announce(TIE);
+    announce(TIE)
     }
 
     // announces which player has won
-    let announce = (type) => { 
-        switch(type) {
+    let announce = (type) => {
+        switch(type){
             case PLAYERO_WON:
                 announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
                 break;
@@ -78,10 +73,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //checks whether a tile has a value or not
     let isValidAction = (tile) => { 
-        if(tile.innerText === 'X' || tile.innerText === 'O') { 
+        if (tile.innerText === 'X' || tile.innerText === 'O'){ 
             return false;
         }
-    }
+        
+        return true;
+    };
 
     //updates the board
     let updateBoard = (index) => {
@@ -89,27 +86,26 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     // changes player based on which turn has just been taken
-    const CHANGE_PLAYER = () => { 
-        let playerDisplay = document.querySelector('.display-player');
+    let changePlayer = () => { 
         playerDisplay.classList.remove(`player${currentPlayer}`);
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
         playerDisplay.innerText = currentPlayer;
         playerDisplay.classList.add(`player${currentPlayer}`);
     }
-    
+
     // represents which user is currently playing. Checks whether game is active or not.
-    const USER_ACTION = (tile, index) => { 
+    let userAction = (tile, index) => { 
         if(isValidAction(tile) && isGameActive) { 
             tile.innerText = currentPlayer;
             tile.classList.add(`player${currentPlayer}`);
             updateBoard(index);
             handleResultValidation();
-            changePlayer;
+            changePlayer();
         }
     }
 
     let resetBoard = () => { 
-        board = ['', '', '','', '', '','', '', ''];
+        board = ['', '', '', '', '', '', '', '', ''];
         isGameActive = true; 
         announcer.classList.add('hide');
 
@@ -123,3 +119,8 @@ window.addEventListener('DOMContentLoaded', () => {
             tile.classList.remove('playerO');
         });
     }
+
+    tiles.forEach( (tile, index) => {
+        tile.addEventListener('click', () => userAction(tile, index));
+    });
+    resetButton.addEventListener('click', resetBoard);
